a741c6fb09c183e340e367f6c84b2e6f32f0082f
Heritorofarc <honproton@protonmail.com>
2026-07-28 21:06:58 -0400

    update src

--- person_add_skill/src/libpersonaddskill_lua.rs
+++ person_add_skill/src/libpersonaddskill_lua.rs
@@ -1,14 +1,13 @@
 use unity::prelude::*;
 use engage::app::eventscript::EventScript;
 use engage::EventScriptExt;
-use engage::{app::{IPersonDataMethods, ISkillDataMethods, PersonData, SkillData}, moon_sharp::interpreter::DynValue};
-extern "C" fn personskillscript(arg: Array<DynValue>, method_info: OptionalMethod){
-  let person = arg.get_string(0);
-  let sid = arg.get_string(1);
-  let person_get = PersonData::get_pid(person);
-  let skill_get = SkillData::get_sid(sid);
-  IPersonDataMethods::set_common_sids(person_get, skill_get);
-  //(xyz, SkillData_Categorys::person(), 0);
+use engage::app::{SkillArray, SkillData_Categorys};
+use engage::{app::{IPersonDataMethods, ISkillDataMethods, IUnitMethods}, moon_sharp::interpreter::DynValue};
+extern "C" fn personskillscript(args: Array<DynValue>, method_info: OptionalMethod){
+  let person = args.get_pid(0);
+  let sid = args.get_sid(1);
+  person.add_skill_2(sid, SkillArray::add_2_method_info(), SkillData_Categorys::person());
+  //
 }
 pub fn register(event: EventScript) {
     event.register_action("PersonAddSkill", personskillscript);
